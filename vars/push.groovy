716833de2c4b486dd22544500df5db17735ac1fb
Dharmik6096 <139250097+Dharmik6096@users.noreply.github.com>
2026-01-22 16:20:45 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -8,9 +8,9 @@ def call(String project,String imagetag){
                 passwordVariable: 'DOCKER_HUB_PASSWORD'
             )
         ]) {
-            sh 'docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD'
-            sh 'docker tag django_app:latest $DOCKER_HUB_USERNAME/django_app:latest'
-            sh 'docker push $DOCKER_HUB_USERNAME/"${project}":"${imagetag}"'
+            sh "docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD"
+            sh "docker tag django_app:latest $DOCKER_HUB_USERNAME/${project}:${imagetag}"
+            sh "docker push $DOCKER_HUB_USERNAME/${project}:${imagetag}"
         }
 
         echo "Push successfully"
