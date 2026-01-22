488d5dde399f5061e76f7feed921a3ccfee5a2bf
Dharmik6096 <139250097+Dharmik6096@users.noreply.github.com>
2026-01-22 16:00:38 +0530

    Create push.groovy

---
+++ vars/push.groovy
@@ -0,0 +1,17 @@
+def call(){
+          echo "Push start"
+
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'django-app',
+                usernameVariable: 'DOCKER_HUB_USERNAME',
+                passwordVariable: 'DOCKER_HUB_PASSWORD'
+            )
+        ]) {
+            sh 'docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD'
+            sh 'docker tag django_app:latest $DOCKER_HUB_USERNAME/django_app:latest'
+            sh 'docker push $DOCKER_HUB_USERNAME/django_app:latest'
+        }
+
+        echo "Push successfully"
+}
